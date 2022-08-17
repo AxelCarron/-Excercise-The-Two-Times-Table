a65ab57ae66c340310d001db5d2e844b8f6519fa
AxelCarron <110579253+AxelCarron@users.noreply.github.com>
2022-08-17 16:23:19 -0300

    Add files via upload

---
+++ Two Times Table/Guia 6.4/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Guia_6._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstTabla = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstTabla
+            // 
+            this.lstTabla.BackColor = System.Drawing.SystemColors.MenuText;
+            this.lstTabla.ForeColor = System.Drawing.Color.Lime;
+            this.lstTabla.FormattingEnabled = true;
+            this.lstTabla.Location = new System.Drawing.Point(12, 12);
+            this.lstTabla.Name = "lstTabla";
+            this.lstTabla.Size = new System.Drawing.Size(260, 95);
+            this.lstTabla.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Black;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Lime;
+            this.btnCalcular.Location = new System.Drawing.Point(13, 114);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(259, 39);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 165);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lstTabla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstTabla;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Two Times Table/Guia 6.4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_6._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        int Dos = 2;
+        int Incremento = 0;
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            
+            int resultado = 0;
+            IncrementoUno (ref Incremento);
+            Calcular(resultado,Incremento);
+            
+            
+        }
+        private void IncrementoUno(ref int incremento)
+        {
+            incremento = incremento + 1;
+        }
+        
+        private void Calcular (int resultado, int incremento1)
+        {
+                     
+            resultado = Dos * incremento1;
+            lstTabla.Items.Add(Dos + "X" + incremento1 + "= " + resultado);
+        }
+        
+
+    }
+
+}
